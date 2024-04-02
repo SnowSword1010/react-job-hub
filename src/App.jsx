27f80eced73be3100dd9ae15c58ecc01d5dcb484
SnowSword1010 <jmayank2001@gmail.com>
2024-04-03 02:02:18 +0530

    :hammer: Router added + separated app into pages and layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'
-import Navbar from './components/Navbar'
-import JobListings from './components/JobListings'
-import ViewAllJobs from './components/ViewAllJobs'
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<MainLayout />}>
+            <Route index element={ <HomePage /> } />
+        </Route>
+    )
+);
 
 const App = () => {
     return (
-        <>
-        <Navbar />
-        <Hero title="Dummy Title" subtitle="Dummy Subtitle"/>
-        <HomeCards />
-        <JobListings />
-        <ViewAllJobs />
-        </>
+        <RouterProvider router={router} />
     )
 }
 
